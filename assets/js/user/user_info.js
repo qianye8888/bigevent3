231b7b554a1f909826430bf678658b913d19cbda
Your Name <you@example.com>
2020-10-29 23:32:46 +0800

    m

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,52 @@
+$(function () {
+    var form = layui.form
+    form.verify({
+        nickname: function (value) {
+            if (value.length > 6) {
+                return "昵称长度为1~6位之间！"
+            }
+        }
+    })
+
+
+    initUserInfo()
+    var layer = layui.layer
+    function initUserInfo () {
+        $.ajax({
+            method: "GET",
+            url: "/my/userinfo",
+            success: function (res) {
+                if(res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
+
+    $("#btnReset").on('click', function(e) {
+        e.preventDefault()
+        initUserInfo()
+    })
+
+
+    // 修改用户信息
+    $('.layui-form').on('submit', function (e) {
+        e.preventDefault()
+
+        $.ajax({
+            method: "POST",
+            url: "/my/userinfo",
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg("用户信息修改失败！")
+                }
+                layer.msg("用户信息修改成功！")
+
+                window.parent.getUserInfo()
+            }
+        })
+    })
+    
+})
